7f27b28590ad0b1195d8fd8ffc5f0753809685bb
Pingvu <nhlpool27@gmail.com>
2023-01-20 14:01:56 +0100

    Add project files.

---
+++ uppgift 5.2 test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Uppgift_5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tal = new string[3];
+            tal[0] = "5";
+            tal[1] = "6";
+            Console.WriteLine($"Skriv in ditt heltal förutom {tal[0]} och {tal[1]}");
+            tal[2] = Console.ReadLine();
+
+
+            Console.WriteLine("Här är de 3 heltalen");
+            Console.WriteLine($"{tal[0]}, {tal[1]} och {tal[2]}");
+
+        }
+    }
+}
